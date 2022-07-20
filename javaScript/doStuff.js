d732ff24bd9f1d146a78d93e51ebabf7c7ae086a
Sara <sarahoubertz@gmail.com>
2022-07-20 14:05:01 +0200

    javaScript structure and functions

---
+++ javaScript/doStuff.js
@@ -0,0 +1,25 @@
+let myLibrary = []; //array for storing books
+
+function Book() {
+    //constructor
+}
+
+function addBookToLibrary() {
+    
+}
+
+function displayBooksInLibrary() {
+    //display all books, maybe on individual cards that pop up
+}
+
+//change read status
+function changeReadStatus() {
+    //check if read button has been clicked, check current status, change 
+    //to other status accordingly
+}
+//remove books
+
+function remove() {
+    //check if remove button on book panel was clicked, if yes, remove panel and
+    //also remove book from array
+}
